b393f9a5250199cdb327e562a17158e3134484c9
Daniil Pomogaev <55309680+Redencon@users.noreply.github.com>
2025-12-03 18:24:44 +0300

    Implemented CRUD + fixes

--- src/primitive_db/core.py
+++ src/primitive_db/core.py
@@ -33,12 +33,16 @@ def insert(metadata, table_name, values):
         raise ValueError(f"Table {table_name} does not exist")
     if len(values) != len(metadata[table_name]) - 1:
         raise ValueError("Invalid number of values")
-    curmax_id = max([v['ID'] for v in metadata[table_name]['values']])
+    if metadata[table_name]['values']:
+        curmax_id = max([v['ID'] for v in metadata[table_name]['values']])
+    else:
+        curmax_id = 0
     dict_to_add =  {'ID': curmax_id+1}
     for value, col in zip(values, metadata[table_name]['columns'][1:]):
-        if type(value) is not value_type_mapping[col[1]]:
-            raise ValueError(f"Invalid value type {type(value)}")
-        dict_to_add[col[0]] = value
+        # if type(value) is not value_type_mapping[col[1]]:
+        #     raise ValueError(f"Invalid value type {type(value)}")
+        dict_to_add[col[0]] = value_type_mapping[col[1]](value)
+    print(f"Добавлена строка: {dict_to_add}")
     metadata[table_name]['values'].append(dict_to_add)
     return metadata
 
@@ -49,11 +53,19 @@ def clause_parser(clause):
     b = ast.literal_eval(b)
     return (a, b)
 
+def get_col_type(metadata, table_name, col_name):
+    col_pair = [col for col in metadata[table_name]['columns'] if col[0] == col_name]
+    if not col_pair:
+        raise ValueError(f"Column {col_name} does not exist")
+    return value_type_mapping[col_pair[0][1]]
+
 def select(metadata, table_name, where=None):
     if table_name not in metadata:
         raise ValueError(f"Table {table_name} does not exist")
     col_to_check, val_to_check = clause_parser(where)
     values = metadata[table_name]['values']
+    if col_to_check:
+        val_to_check = get_col_type(metadata, table_name, col_to_check)(val_to_check)
     return [
         row for row in values if where is None or row[col_to_check] == val_to_check
     ]
@@ -62,17 +74,37 @@ def update(metadata, table_name, set_value, where=None):
     if table_name not in metadata:
         raise ValueError(f"Table {table_name} does not exist")
     a, b = clause_parser(where)
+    b = get_col_type(metadata, table_name, a)(b)
     x, y = clause_parser(set_value)
+    y = get_col_type(metadata, table_name, x)(y)
+    update_ids = []
     for row in metadata[table_name]['values']:
         if where is None or row[a] == b:
             row[x] = y
+            update_ids.append(row['ID'])
+    print("Обновлены строки с ID:", *update_ids)
     return metadata
 
 
-def delete(metadata, where):
+def delete(metadata, table_name, where):
     a, b = clause_parser(where)
-    metadata["values"] = [row for row in metadata["values"] if row[a] != b]
+    b = get_col_type(metadata, table_name, a)(b)
+    update_ids = [row["ID"] for row in metadata[table_name]["values"] if row[a] == b]
+    metadata[table_name]["values"] = [
+        row for row in metadata[table_name]["values"] if row[a] != b
+    ]
+    print("Удалены строки с ID:", *update_ids)
     return metadata
+
+
+def info(metadata, table_name):
+    if table_name not in metadata:
+        raise ValueError(f"Table {table_name} does not exist")
+    print(f"""
+Таблица: {table_name}
+Столбцы: {', '.join([col[0]+':'+col[1] for col in metadata[table_name]["columns"]])}
+Количество записей: {len(metadata[table_name]["values"])}
+""")
     
     
 
--- src/primitive_db/engine.py
+++ src/primitive_db/engine.py
@@ -1,18 +1,34 @@
 """Running the app on the command line"""
 import shlex
+from typing import Any
 
 import prompt
 
-from .core import create_table, drop_table
+from .core import create_table, delete, drop_table, info, insert, select, update
 from .utils import DEFAULT_META_FILE, load_metadata, save_metadata
 
 HELP_TEXT = """
-<command> create_table <имя_таблицы> <столбец1:тип> <столбец2:тип> .. - создать таблицу
-<command> list_tables - показать список всех таблиц
-<command> drop_table <имя_таблицы> - удалить таблицу
-<command> exit - выйти из программы
+<command> insert into <имя_таблицы> values (<значение1>, <значение2>, ...) \
+- создать запись.
+<command> select from <имя_таблицы> where <столбец> = <значение> \
+- прочитать записи по условию.
+<command> select from <имя_таблицы> - прочитать все записи.
+<command> update <имя_таблицы> set <столбец1> = <новое_значение1> \
+where <столбец_условия> = <значение_условия> - обновить запись.
+<command> delete from <имя_таблицы> where <столбец> = <значение> - удалить запись.
+<command> info <имя_таблицы> - вывести информацию о таблице.
+<command> exit - выход из программы
 <command> help - справочная информация
 """
+
+def print_rows(rows: list[dict[str, Any]]):
+    print("\n".join([
+        ", ".join([
+            f"{k}: {v}" for k, v in row.items()
+        ])
+        for row in rows
+    ]))
+    
 
 def run():
     while True:
@@ -40,6 +56,46 @@ def run():
                     drop_table(metadata, table_name)
                 case "list_tables":
                     print(*metadata.keys())
+                case "insert":
+                    if args[0] != "into":
+                        raise ValueError("Unknown syntax for INSERT command")
+                    table_name = args[1]
+                    if args[2] != "values":
+                        raise ValueError("Unknown syntax for INSERT command")
+                    values = args[3:]
+                    metadata = insert(metadata, table_name, values)
+                case "select":
+                    if args[0] != "from":
+                        raise ValueError("Unknown syntax for SELECT command")
+                    table_name = args[1]
+                    if "where" not in args:
+                        rows = select(metadata, table_name)
+                        print_rows(rows)
+                    else:
+                        clause = " ".join(args[3:6])
+                        rows = select(metadata, table_name, clause)
+                        print_rows(rows)
+                case "update":
+                    table_name = args[0]
+                    if args[1] != "set":
+                        raise ValueError("Unknown syntax for UPDATE command")
+                    set_clause = " ".join(args[2:5])
+                    if "where" not in args:
+                        metadata = update(metadata, table_name, set_clause)
+                    else:
+                        where_clause = " ".join(args[6:9])
+                        metadata = update(
+                            metadata, table_name, set_clause, where_clause
+                        )
+                case "delete":
+                    if args[0] != "from":
+                        raise ValueError("Unknown syntax for DELETE command")
+                    table_name = args[1]
+                    clause = " ".join(args[3:6])
+                    metadata = delete(metadata, table_name, clause)
+                case "info":
+                    table_name = args[0]
+                    info(metadata, table_name)
                 case _:
                     print(
                         "Unknown command. "
